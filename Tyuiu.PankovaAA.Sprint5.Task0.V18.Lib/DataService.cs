f724119e8ea73b0567060b7e3c3c6add94eaeb88
Ангелина Панькова <Tyuiu.PankovaAA@outlook.com>
2025-11-19 12:20:25 +0500

    Task 0

--- Tyuiu.PankovaAA.Sprint5.Task0.V18.Lib/DataService.cs
+++ Tyuiu.PankovaAA.Sprint5.Task0.V18.Lib/DataService.cs
@@ -1,18 +1,17 @@
-﻿using System;
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+using System;
 using System.IO;
-using tyuiu.cources.programming.interfaces.Sprint5;
-
 
 namespace Tyuiu.PankovaAA.Sprint5.Task0.V18.Lib
 {
-    public class DataService : ISprint5Task0V18
+    public class DataService: ISprint5Task0V18
     {
         public string SaveToFileTextData(int x)
         {
-            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt";
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask0.txt");
             double z = x / Math.Sqrt(x * x + x);
-            z = Math.Round(z, 2);
-            File.WriteAllText(path, Convert.ToString(z));
+            z = Math.Round(z, 3); 
+            File.WriteAllText(path, z.ToString());
             return path;
         }
     }
--- Tyuiu.PankovaAA.Sprint5.Task0.V18.Test/DataServiceTest.cs
+++ Tyuiu.PankovaAA.Sprint5.Task0.V18.Test/DataServiceTest.cs
@@ -3,15 +3,18 @@ using Tyuiu.PankovaAA.Sprint5.Task0.V18.Lib;
 namespace Tyuiu.PankovaAA.Sprint5.Task0.V18.Test
 {
     [TestClass]
-    public sealed class DataServiceTest
+    public class DataServiceTest
     {
         [TestMethod]
         public void TestMethod1()
         {
-            string path = @"C:\Users\ACER\OneDrive\Desktop\Tyuiu.PankovaAA.Sprint5\Debug\OutPutFileTask0.txt";
+            DataService ds = new DataService();
+            string path = ds.SaveToFileTextData(3);
+
             FileInfo fileInfo = new FileInfo(path);
             bool fileExists = fileInfo.Exists;
             bool wait = true;
+
             Assert.AreEqual(wait, fileExists);
         }
     }
