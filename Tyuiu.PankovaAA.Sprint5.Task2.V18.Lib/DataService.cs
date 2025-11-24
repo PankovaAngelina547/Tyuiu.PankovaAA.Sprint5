7d0c4aab08296a01a2164023529c4fa78ae51aa4
Ангелина Панькова <Tyuiu.PankovaAA@outlook.com>
2025-11-24 14:03:00 +0500

    Task 2

--- Tyuiu.PankovaAA.Sprint5.Task2.V18.Lib/DataService.cs
+++ Tyuiu.PankovaAA.Sprint5.Task2.V18.Lib/DataService.cs
@@ -8,37 +8,25 @@ namespace Tyuiu.PankovaAA.Sprint5.Task2.V18.Lib
         {
             string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask2.csv");
 
-            FileInfo fileInfo = new FileInfo(path);
-            bool fileExists = fileInfo.Exists;
-
-            if (fileExists)
+            if (File.Exists(path))
             {
                 File.Delete(path);
             }
 
-            int rows = matrix.GetLength(0); 
-            int columns = matrix.GetLength(1); 
-
             using (StreamWriter writer = new StreamWriter(path))
             {
-                for (int i = 0; i < rows; i++)
+                for (int i = 0; i < 3; i++)
                 {
-                    string line = "";
-                    for (int j = 0; j < columns; j++)
-                    {
-                        
-                        int value = (matrix[i, j] > 0) ? 1 : 0;
+                    writer.Write(matrix[i, 0] > 0 ? "1" : "0");
+                    writer.Write(";");
+                    writer.Write(matrix[i, 1] > 0 ? "1" : "0");
+                    writer.Write(";");
+                    writer.Write(matrix[i, 2] > 0 ? "1" : "0");
 
-                        if (j < columns - 1)
-                        {
-                            line += value + "; ";
-                        }
-                        else
-                        {
-                            line += value;
-                        }
+                    if (i < 2)
+                    {
+                        writer.WriteLine();
                     }
-                    writer.WriteLine(line);
                 }
             }
 
@@ -46,5 +34,3 @@ namespace Tyuiu.PankovaAA.Sprint5.Task2.V18.Lib
         }
     }
 }
-
-
