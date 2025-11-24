0c069cf3ec6ff3f79dc7bbc43a8d2639403364d8
Ангелина Панькова <Tyuiu.PankovaAA@outlook.com>
2025-11-24 13:33:24 +0500

    Task 1

--- Tyuiu.PankovaAA.Sprint5.Task1.V16.Lib/DataService.cs
+++ Tyuiu.PankovaAA.Sprint5.Task1.V16.Lib/DataService.cs
@@ -1,5 +1,6 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint5;
+﻿using System.Globalization;
 using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
 namespace Tyuiu.PankovaAA.Sprint5.Task1.V16.Lib
 {
     public class DataService : ISprint5Task1V16
@@ -8,25 +9,26 @@ namespace Tyuiu.PankovaAA.Sprint5.Task1.V16.Lib
         {
             string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask1.txt");
 
-            FileInfo fileInfo = new FileInfo(path);
-            bool fileExists = fileInfo.Exists;
-
-            if (fileExists)
+            if (File.Exists(path))
             {
                 File.Delete(path);
             }
 
             using (StreamWriter writer = new StreamWriter(path))
             {
-                writer.WriteLine("Табулирование функции F(x) = sin(x) + 2x/3 - cos(x)*4x");
-                writer.WriteLine($"Диапазон: [{startValue}; {stopValue}]");
-                writer.WriteLine("x\t\tf(x)");
-                writer.WriteLine("----------------------");
-
                 for (int x = startValue; x <= stopValue; x++)
                 {
                     double y = CalculateFunction(x);
-                    writer.WriteLine($"{x}\t\t{y:F2}");
+                    string result = y.ToString("F2", CultureInfo.InvariantCulture); 
+
+                    if (x < stopValue)
+                    {
+                        writer.WriteLine(result);
+                    }
+                    else
+                    {
+                        writer.Write(result);
+                    }
                 }
             }
 
@@ -41,8 +43,7 @@ namespace Tyuiu.PankovaAA.Sprint5.Task1.V16.Lib
                 double term2 = (2.0 * x) / 3.0;
                 double term3 = Math.Cos(x) * 4 * x;
 
-                double result = term1 + term2 - term3;
-                return Math.Round(result, 2);
+                return Math.Round(term1 + term2 - term3, 2);
             }
             catch (DivideByZeroException)
             {
