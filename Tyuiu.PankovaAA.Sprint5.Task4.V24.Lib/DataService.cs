6b722aaa8b3442b1080816aaeafb1162afdd26e9
Ангелина Панькова <Tyuiu.PankovaAA@outlook.com>
2025-11-25 14:24:50 +0500

    Task 4

---
+++ Tyuiu.PankovaAA.Sprint5.Task4.V24.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.PankovaAA.Sprint5.Task4.V24.Lib
+{
+    public class DataService : ISprint5Task4V24
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string text = File.ReadAllText(path);
+            text = text.Replace(".", ",");
+            double x = Convert.ToDouble(text);
+
+            double result = (1 / (x * x) + 2) * Math.Sin(x);
+            return Math.Round(result, 3);
+
+        }
+    }
+    
+}
---
+++ Tyuiu.PankovaAA.Sprint5.Task4.V24/Program.cs
@@ -0,0 +1,46 @@
+﻿using Tyuiu.PankovaAA.Sprint5.Task4.V24.Lib;
+namespace Tyuiu.PankovaAA.Sprint5.Task4.V24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            string path = @"C:\DataSprint5\InPutDataFileTask4V24.txt";
+
+            Console.Title = "Спринт #5 [Выполнила: Панькова А.А.] ПИН6-25-1";
+            Console.WriteLine("*******************************************************************************");
+            Console.WriteLine("* Спринт #5                                                                  *");
+            Console.WriteLine("* Тема:  Чтение данных из текстового файла                                   *");
+            Console.WriteLine("* Задание #04                                                                *");
+            Console.WriteLine("* Вариант #24                                                                *");
+            Console.WriteLine("* Выполнила: Панькова Ангелина Алексеевна | ПИН6-25-1                        *");
+            Console.WriteLine("*******************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                   *");
+            Console.WriteLine("*                                                                            *");
+            Console.WriteLine("*                                                                            *");
+            Console.WriteLine("*                                                                            *");
+            Console.WriteLine("*                                                                            *");
+            Console.WriteLine("*******************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                           *");
+            Console.WriteLine("*******************************************************************************");
+
+
+            Console.WriteLine("*******************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                 *");
+            Console.WriteLine("*******************************************************************************");
+            if (!File.Exists(path))
+            {
+                Directory.CreateDirectory(@"C:\DataSprint5\");
+                File.WriteAllText(path, "2.0");
+            }
+
+
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine("Результат: " + res);
+            Console.ReadKey();
+
+            Console.ReadKey();
+        }
+    }
+}
